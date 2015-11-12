aeffed810444bc44799e8afde15b28f404853e0d
kalenakudelima <nmastudent@KOAMAC004.local>
2015-11-12 11:18:36 -1000

    Exercise

--- week-12/exercise/script.v2.js
+++ week-12/exercise/script.v2.js
@@ -1,9 +1,9 @@
 // Create an array including the first names of each student in the class
-var classMates;
+var classMates = ['Sam','Sasha','Hyela','Volha','Myoung','Mayu','Yosh','Mark','Jae','Kalena'];
 
 // Student status string
 var studentStatus = ' is an interface design student.';
 
 // Display in the console the status of each student,
-// using a for loop and one console statement. 
+// using a for loop and one console statement.
 console.log(classMates + studentStatus);
